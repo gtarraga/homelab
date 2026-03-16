8affe56542bef56a6b4acf41e2ea7d2e1ff8ff21
gtarraga <gt.tarraga@gmail.com>
2026-03-16 20:52:31 +0100

    feat: update SSM parameters to ignore changes for Cloudflare tokens
    - Added lifecycle block to ignore changes for the value of Cloudflare external DNS and Terraform tokens in SSM parameters, preventing overwrites during Terraform applies.

--- terraform/aws/parameters.tf
+++ terraform/aws/parameters.tf
@@ -2,10 +2,20 @@ resource "aws_ssm_parameter" "cf_externaldns_token" {
   name  = "/homelab/cloudflare/externaldns/token"
   type  = "SecureString"
   value = "bootstrap-replace-me" # This gets replaced by the actual token via aws ssm put-parameter
+
+  # Ignoring changes as any other terraform applies would overwrite the token
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "cf_terraform_token" {
   name  = "/homelab/cloudflare/terraform/token"
   type  = "SecureString"
   value = "bootstrap-replace-me" # This gets replaced by the actual token via aws ssm put-parameter
+
+  # Ignoring changes as any other terraform applies would overwrite the token
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
